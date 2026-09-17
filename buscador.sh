78c5df4264312e8037a47cad4c12764eda041e18
Beny Vazquez <dbvl0803@gmail.com>
2026-09-17 12:07:57 -0600

    Primera versión pasos 1 y 2

--- buscador.sh
+++ buscador.sh
@@ -1,1 +1,15 @@
-#Inicio del buscador
+#!/bin/bash
+#Recibir Carpeta
+nombreCarpeta=$1
+
+
+if [$# -lt 1 ]; then
+	echo "No se introdujeron argumentos... Saliendo"
+	exit 1
+elif [ ! -d "$nombreCarpeta" ]; then
+	echo "El argumento no es una carpeta... Saliendo"
+	exit 1
+else 
+	grep -rl "home" "$nombreCarpeta"
+fi
+
